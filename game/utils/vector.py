b1b70ee49fa9aaf7a6ce665f6def0b3d71c82019
Plrrb <plrrb@protonmail.com>
2022-03-31 15:57:37 -0700

    Vector now inherits from tuple

--- game/utils/vector.py
+++ game/utils/vector.py
@@ -1,170 +1,151 @@
 from math import sqrt
 
 
-class __Vector__:
+class __Vector__(tuple):
     def inbounds(self, v1, v2):
         return v1 < self < v2
 
     def length(self):
         return sum(abs(self))
 
+    @property
+    def x(self):
+        return self[0]
+
+    @property
+    def y(self):
+        return self[1]
+
 
 class Vector3(__Vector__):
-    __slots__ = "x", "y", "z"
+    def __new__(cls, x, y, z):
+        return super().__new__(Vector3, (x, y, z))
+
+    @property
+    def z(self):
+        return self[2]
 
     def normal(self):
         n = 1 / sum(self)
-        return self * Vector3(n, n, n)
+        return self * (n, n, n)
 
     def distance(self, other):
         d = abs(self - other)
-
-        return sqrt(sum(d ** Vector3(2, 2, 2)))
-
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def __repr__(self):
-        return f"{self.x}, {self.y}, {self.z}"
+        return sqrt(sum(d ** (2, 2, 2)))
 
     def __add__(self, other):
-        return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+        return Vector3(self[0] + other[0], self[1] + other[1], self[2] + other[2])
 
     def __sub__(self, other):
-        return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
+        return Vector3(self[0] - other[0], self[1] - other[1], self[2] - other[2])
 
     def __mul__(self, other):
-        return Vector3(self.x * other.x, self.y * other.y, self.z * other.z)
+        return Vector3(self[0] * other[0], self[1] * other[1], self[2] * other[2])
 
     def __truediv__(self, other):
-        return Vector3(self.x / other.x, self.y / other.y, self.z / other.z)
+        return Vector3(self[0] / other[0], self[1] / other[1], self[2] / other[2])
 
     def __floordiv__(self, other):
-        return Vector3(self.x // other.x, self.y // other.y, self.z // other.z)
+        return Vector3(self[0] // other[0], self[1] // other[1], self[2] // other[2])
 
     def __mod__(self, other):
-        return Vector3(self.x % other.x, self.y % other.y, self.z % other.z)
+        return Vector3(self[0] % other[0], self[1] % other[1], self[2] % other[2])
 
     def __pow__(self, other):
-        return Vector3(self.x ** other.x, self.y ** other.y, self.z ** other.z)
+        return Vector3(self[0] ** other[0], self[1] ** other[1], self[2] ** other[2])
 
     def __lt__(self, other):
-        return self.x < other.x and self.y < other.y and self.z < other.z
+        return self[0] < other[0] and self[1] < other[1] and self[2] < other[2]
 
     def __le__(self, other):
-        return self.x <= other.x and self.y <= other.y and self.z <= other.z
+        return self[0] <= other[0] and self[1] <= other[1] and self[2] <= other[2]
 
     def __gt__(self, other):
-        return self.x > other.x and self.y > other.y and self.z > other.z
+        return self[0] > other[0] and self[1] > other[1] and self[2] > other[2]
 
     def __ge__(self, other):
-        return self.x >= other.x and self.y >= other.y and self.z >= other.z
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y and self.z == other.z
-
-    def __ne__(self, other):
-        return self.x != other.x and self.y != other.y and self.z != other.z
+        return self[0] >= other[0] and self[1] >= other[1] and self[2] >= other[2]
 
     def __neg__(self):
-        return Vector3(-self.x, -self.y, -self.z)
+        return Vector3(-self[0], -self[1], -self[2])
 
     def __pos__(self):
-        return Vector3(+self.x, +self.y, +self.z)
+        return Vector3(+self[0], +self[1], +self[2])
 
     def __invert__(self):
-        return Vector3(~self.x, ~self.y, ~self.z)
+        return Vector3(~self[0], ~self[1], ~self[2])
 
     def __bool__(self):
-        return self.x != 0 and self.y != 0 and self.z != 0
+        return self[0] != 0 and self[1] != 0 and self[2] != 0
 
     def __abs__(self):
-        return Vector3(abs(self.x), abs(self.y), abs(self.z))
+        return Vector3(abs(self[0]), abs(self[1]), abs(self[2]))
 
     def __round__(self, digit):
-        return Vector3(round(self.x, digit), round(self.y, digit), round(self.z, digit))
-
-    def __getitem__(self, index):
-        return (self.x, self.y, self.z)[index]
+        return Vector3(
+            round(self[0], digit), round(self[1], digit), round(self[2], digit)
+        )
 
 
 class Vector2(__Vector__):
-    __slots__ = "x", "y"
+    def __new__(cls, x, y):
+        return super().__new__(Vector2, (x, y))
 
     def normal(self):
         n = 1 / sum(self)
-        return self * Vector2(n, n)
+        return self * (n, n)
 
     def distance(self, other):
         d = abs(self - other)
-
-        return sqrt(sum(d ** Vector2(2, 2)))
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __repr__(self):
-        return f"{self.x}, {self.y}"
+        return sqrt(sum(d ** (2, 2)))
 
     def __add__(self, other):
-        return Vector2(self.x + other.x, self.y + other.y)
+        return Vector2(self[0] + other[0], self[1] + other[1])
 
     def __sub__(self, other):
-        return Vector2(self.x - other.x, self.y - other.y)
+        return Vector2(self[0] - other[0], self[1] - other[1])
 
     def __mul__(self, other):
-        return Vector2(self.x * other.x, self.y * other.y)
+        return Vector2(self[0] * other[0], self[1] * other[1])
 
     def __truediv__(self, other):
-        return Vector2(self.x / other.x, self.y / other.y)
+        return Vector2(self[0] / other[0], self[1] / other[1])
 
     def __floordiv__(self, other):
-        return Vector2(self.x // other.x, self.y // other.y)
+        return Vector2(self[0] // other[0], self[1] // other[1])
 
     def __mod__(self, other):
-        return Vector2(self.x % other.x, self.y % other.y)
+        return Vector2(self[0] % other[0], self[1] % other[1])
 
     def __pow__(self, other):
-        return Vector2(self.x ** other.x, self.y ** other.y)
+        return Vector2(self[0] ** other[0], self[1] ** other[1])
 
     def __lt__(self, other):
-        return self.x < other.x and self.y < other.y
+        return self[0] < other[0] and self[1] < other[1]
 
     def __le__(self, other):
-        return self.x <= other.x and self.y <= other.y
+        return self[0] <= other[0] and self[1] <= other[1]
 
     def __gt__(self, other):
-        return self.x > other.x and self.y > other.y
+        return self[0] > other[0] and self[1] > other[1]
 
     def __ge__(self, other):
-        return self.x >= other.x and self.y >= other.y
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __ne__(self, other):
-        return self.x != other.x and self.y != other.y
+        return self[0] >= other[0] and self[1] >= other[1]
 
     def __neg__(self):
-        return Vector3(-self.x, -self.y)
+        return Vector3(-self[0], -self[1])
 
     def __pos__(self):
-        return Vector3(+self.x, +self.y)
+        return Vector3(+self[0], +self[1])
 
     def __invert__(self):
-        return Vector3(~self.x, ~self.y)
+        return Vector3(~self[0], ~self[1])
 
     def __bool__(self):
-        return self.x != 0 and self.y != 0
+        return self[0] != 0 and self[1] != 0
 
     def __abs__(self):
-        return Vector2(abs(self.x), abs(self.y))
+        return Vector2(abs(self[0]), abs(self[1]))
 
     def __round__(self, digit):
-        return Vector2(round(self.x, digit), round(self.y, digit))
-
-    def __getitem__(self, index):
-        return (self.x, self.y)[index]
+        return Vector2(round(self[0], digit), round(self[1], digit))
